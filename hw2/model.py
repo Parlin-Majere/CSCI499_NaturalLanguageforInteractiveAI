1f0e29b83333bc4cb9de7414db2ea791851682cc
Jiaqi Lu <jiaqilu@usc.edu>
2022-10-25 20:48:06 -0700

    updated model
    updated based on comments made.

--- hw2/model.py
+++ hw2/model.py
@@ -9,14 +9,10 @@ class CBOW(nn.Module):
             num_embeddings = self.vocab_size,
             embedding_dim = self.embedding_dim,
         )
-        self.linear1 = nn.Linear(self.embedding_dim,128)
-        self.activation_function1 = nn.ReLU()
-        self.linear2 = nn.Linear(128,self.vocab_size)
+        self.linear1 = nn.Linear(self.embedding_dim,self.vocab_size)
     def forward(self, input):
         embeds = self.embed(input)
         embeds = embeds.mean(axis=1)
         out1 = self.linear1(embeds)
-        out1 = self.activation_function1(out1)
-        out2 = self.linear2(out1)
 
-        return out2
+        return out1
